e60bec2a21433f5a578bd7d8fbed832ee499b2e3
Moulesh T <moulesh23@gmail.com>
2020-09-17 13:12:54 +0530

    Add files via upload

---
+++ Example_Programs/Factorial/inc/check.h
@@ -0,0 +1,16 @@
+/**
+* @file factorial.h
+*
+*/
+#ifndef __CHECK_H__
+#define __CHECK_H__
+
+/**
+* Calculates the factorial of integer number
+* @param[in] number for which factorial has to be found
+* @return Factorial of the number
+* @note Returns -1 for negative values
+*/
+int check(int number);
+
+#endif 
